236a6ba3617754a0bd379a0cbeec664d0921db1e
Dhruba <binodtharu977@gmail.com>
2025-05-14 12:50:55 +0545

    logo uploaded

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -3,39 +3,26 @@ import React from "react";
 const Navbar = () => {
     return (
         <>
-            <nav style={{ background: "#333", padding: "1rem" }}>
-                <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0 }}>
-                    <li style={{ marginRight: "1.5rem" }}>
-                        <a href="/" style={{ color: "#fff", textDecoration: "none" }}>
-                            Home
-                        </a>
-                    </li>
-                    <li style={{ marginRight: "1.5rem" }}>
-                        <a href="/jobs" style={{ color: "#fff", textDecoration: "none" }}>
-                            Jobs
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/about" style={{ color: "#fff", textDecoration: "none" }}>
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/contact" style={{ color: "#fff", textDecoration: "none" }}>
-                            Contact Us
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/login" style={{ color: "#fff", textDecoration: "none" }}>
-                            Login
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/register" style={{ color: "#fff", textDecoration: "none" }}>
-                            Register
-                        </a>
-                    </li>
-                </ul>
+            <nav className="flex justify-between items-center">
+                <div>
+                    <img src="../assests/logo.png" alt="Logo" />
+                </div>
+
+                <div>
+                    <ul>
+                        <li className="">
+                            <a className="">Home</a>
+                            <a className="">Jobs</a>
+                            <a className="">About Us</a>
+                            <a className="">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <button> Login </button>
+                    <button> Register </button>
+                </div>
             </nav>
         </>
     );
